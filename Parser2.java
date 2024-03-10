d988dc17ae10170024ddf9c0fb6fb71ca8fa8c3a
unknown <nathan.d.borchelt@gmail.com>
2024-03-10 14:34:46 -0500

    fixedx EOF

--- Parser2.java
+++ Parser2.java
@@ -33,16 +33,15 @@ public class Parser2 {
     }
 
     private void match(String expected) {
-        if (compare(expected)) {
+
+        if (current == null && expected.equals("EOF")) {
+            System.out.println("<EOF>");
+            System.exit(0);
+        } else if (compare(expected)) {
             System.out.println("Matched: " + current.getLexeme());
             current = lexer.lex();
             if (current != null)
                 ;
-
-            else {
-                System.out.println("EOF");
-                System.exit(0);
-            }
         }
 
         else {
@@ -81,6 +80,8 @@ public class Parser2 {
         begin("program");
 
         stmt_list();
+
+        match("EOF");
     }
 
     // <stmt_list> -> <stmt> { <stmt> }
